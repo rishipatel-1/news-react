82a267c0f645cd260267e997ada9e4dc431b397d
Rishi Patel <riishipatel.2.1@gmail.com>
2023-03-15 14:25:46 +0530

    routes added to news app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
    <>
+<BrowserRouter>
 <Navbar/>
-<News category="business"/>
+    <Routes>
+      <Route exact path='/' element={<News category="sports"/>} />
+      <Route exact path="/science" element={<News key="science" category="science"/>}/> 
+      <Route exact path="/sports" element={<News key="sports" category="sports"/>}  /> 
+      <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment"/>}/> 
+      <Route exact path="/business" element={<News key="business" category="business"/>}  /> 
+      <Route exact path="/health" element={<News key="health" category="health"/>}/> 
+      <Route exact path="/technology" element={<News key="technology" category="technology"/>}  /> 
+          
+    </Routes>
+</BrowserRouter>
    </>
   );
 }
